038fc1cf480ecd2e9e1d65f2e43df33ec82d736e
JHarGit <jonnythebob@gmail.com>
2025-03-04 16:33:56 +0100

    downgraded lombok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,9 @@ module elevator.elevatorgui {
 
     requires org.controlsfx.controls;
     requires com.fazecast.jSerialComm;
-    requires lombok;
     requires org.apache.logging.log4j;
     requires org.apache.logging.log4j.core;
-    opens gui to javafx.fxml;
+    requires lombok;
     exports gui;
+    opens gui to javafx.fxml;
 }
